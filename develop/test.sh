3bc49b912fec54ae4b3b94fcb712a94418b15bb3
Shashank Sharma <shashankrnr32@gmail.com>
2021-07-02 13:37:34 +0530

    Remove Testplan Playground

--- develop/test.sh
+++ develop/test.sh
@@ -3,7 +3,7 @@ function unittest() {
     echo "==============================================="
     echo "Running Unittests with coverage"
     pytest --cov=ychaos --cov-report term-missing tests --durations=5
-    rm .coverage*
+    rm .coverage .coverage.*
 }
 
 unittest

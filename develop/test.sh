0f37b7e04d40833063f80c73166df5c247067d61
Shashank Sharma <shashankrnr32@gmail.com>
2021-07-13 22:08:59 +0530

    Reduce UT execution time

--- develop/test.sh
+++ develop/test.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+
 # Unittest and coverage using pytest
 function unittest() {
     echo "==============================================="
     echo "Running Unittests with coverage"
-    pytest --cov=ychaos --cov-report term-missing tests --durations=5
+    pytest --cov=ychaos --cov-report term-missing tests --durations=5 -n 3 --dist loadfile
 }
 
 unittest

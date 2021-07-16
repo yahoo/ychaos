c77423dba8eb8b7499a79dc4553e7f2652e7f196
lakshmi-k05 <kplakshmi.97@gmail.com>
2021-07-16 16:03:05 +0530

    Adding split filler files

--- src/ychaos/agents/system/diskFill.py
+++ src/ychaos/agents/system/diskFill.py
@@ -1,23 +1,19 @@
-#  Copyright 2021, Verizon Media
-#  Licensed under the terms of the ${MY_OSI} license. See the LICENSE file in the project root for terms
-
+#  Copyright 2021, Yahoo
+#  Licensed under the terms of the Apache 2.0 license. See the LICENSE file in the project root for terms
 import math
 import shutil
-
-from queue import LifoQueue
 from pathlib import Path
+from queue import LifoQueue
+
 from pydantic import Field
 
-from ..agent import Agent, AgentMonitoringDataPoint, TimedAgentConfig, AgentPriority
+from ..agent import (
+    Agent,
+    AgentMonitoringDataPoint,
+    AgentPriority,
+    TimedAgentConfig,
+)
 from ..utils.annotations import log_agent_lifecycle
-
-
-def _fill_disk(size: int, partition: Path):
-    f = open(partition/'filler.txt', "wb")
-    f.seek(size-1)
-    f.write(b"\0")
-    f.close()
-    # TODO: Implement max file size to split required disk fill
 
 
 class DiskFillConfig(TimedAgentConfig):
@@ -27,14 +23,15 @@ class DiskFillConfig(TimedAgentConfig):
     space, as defined in the `partition_pct` attribute. By default,
     `partition_pct` is set to 100, implying the complete disk space of that partition will be filled.
     """
+
     name = "disk_fill"
     description = "This agent fills up disk space."
 
     priority = AgentPriority.MODERATE_PRIOTITY
-    is_sudo = True
 
     partition: Path = Field(
-        description="The Filepath of directory or partition to fill", default=Path("/etc/")
+        description="The Filepath of directory or partition to fill",
+        default=Path("/etc/"),
     )
 
     partition_pct: float = Field(
@@ -43,7 +40,7 @@ class DiskFillConfig(TimedAgentConfig):
             "Percentage of the disk partition to fill"
             "This will fill a percentage of the disk space on the partition"
         ),
-        ge=0,
+        gt=0,
         le=100,
     )
 
@@ -58,8 +55,16 @@ class DiskFillConfig(TimedAgentConfig):
         partition_size_available = stat.free
         return math.floor(self.partition_pct * partition_size_available)
 
+    # def _fill_disk(self, size: int, partition: Path):
+    #     f = open(partition/'filler.txt', "wb")
+    #     f.seek(size-1)
+    #     f.write(b"\0")
+    #     f.close()
+    #     # TODO: Implement max file size to split required disk fill
+
 
 class DiskFill(Agent):
+    max_file_size: int = 1024 * 1024 * 1024 * 20  # Max file size is 20GB
 
     def monitor(self) -> LifoQueue:
         super(DiskFill, self).monitor()
@@ -78,8 +83,26 @@ class DiskFill(Agent):
     @log_agent_lifecycle
     def run(self) -> None:
         super(DiskFill, self).run()
+        size = self.config.effective_disk_to_fill()
+        tmp_dir = Path(self.config.partition / "temp")
+        tmp_dir.mkdir(parents=True, exist_ok=True)
+
+        space_remaining = size
+        index = 0
+        while space_remaining > 0:
+            tmp = space_remaining
+            cur_file_size = self.max_file_size
+            space_remaining = space_remaining - self.max_file_size
+            if space_remaining <= 0:
+                cur_file_size = tmp
+            f = open(tmp_dir / "filler{in}.txt".replace("{in}", str(index)), "wb")
+            f.seek(cur_file_size - 1)
+            f.write(b"\0")
+            f.close()
+            index += 1
 
     @log_agent_lifecycle
     def teardown(self) -> None:
         super(DiskFill, self).teardown()
-        # TODO: Cleanup all junk files created
+        tmp_dir = self.config.partition / "temp"
+        shutil.rmtree(tmp_dir)
